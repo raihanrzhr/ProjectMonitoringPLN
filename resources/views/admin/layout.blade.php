a371ca49f34ca640f86f96844b49a70374f3c050
Rafa <rafarashieka@gmail.com>
2025-12-18 09:13:11 +0700

    perubahan fav icon

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Admin - UP2D Pasundan')</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/LOGO_PLN_1.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/LOGO_PLN_1.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/LOGO_PLN_1.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'UP2D Pasundan')</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/LOGO_PLN_1.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/LOGO_PLN_1.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/LOGO_PLN_1.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Playfair+Display:wght@700&display=swap');
